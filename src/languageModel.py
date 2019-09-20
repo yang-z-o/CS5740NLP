0c042d3315cb0d0e936c8f60cb03160649f5dea6
Yangmengyuan Zhao <zymy701@gmail.com>
2019-09-20 15:00:54 -0400

    add-k acc

--- src/languageModel.py
+++ src/languageModel.py
@@ -17,17 +17,23 @@ def main():
 			# smoothing_parameter_training(validation_file, train_count, unicounts, bicounts)
 			# interpolation_smoothing(validation_file, train_count, unicounts, bicounts)
 			kneser_ney_smoothing(validation_file, train_count, unicounts, bicounts)
+	elif len(sys.argv) == 4:
+		# test mode
+		class1, class2= int(sys.argv[1]), int(sys.argv[2])
+		filename = ['deceptive', 'truthful']
+		training_file_path1 = 'DATASET/train/' + filename[class1] + '.txt'
+		training_file_path2 = 'DATASET/train/' + filename[class2] + '.txt'
+		test_file_path = 'DATASET/test/' + sys.argv[3] + '.txt'
+		test(training_file_path1, training_file_path2, test_file_path, class1, class2)
 	elif len(sys.argv) == 5:
 		# validation mode - training accuracy
 		class1, class2, y1, y2 = map(int, sys.argv[1:5])
 		filename = ['deceptive', 'truthful']
 		training_file_path1 = 'DATASET/train/' + filename[class1] + '.txt'
 		training_file_path2 = 'DATASET/train/' + filename[class2] + '.txt'
-		# test_file_path = 'DATASET/test/' + sys.argv[3] + '.txt'
-		# test_file_path = sys.argv[3]
-		test_file_path1 = 'DATASET/validation/' + filename[y1] + '.txt'
-		test_file_path2 = 'DATASET/validation/' + filename[y2] + '.txt'
-		test(training_file_path1, training_file_path2, test_file_path1, test_file_path2, class1, class2, y1, y2)
+		validation_file_path1 = 'DATASET/validation/' + filename[y1] + '.txt'
+		validation_file_path2 = 'DATASET/validation/' + filename[y2] + '.txt'
+		accuracy_training(training_file_path1, training_file_path2, validation_file_path1, validation_file_path2, class1, class2, y1, y2)
 	else:
 		print('Please input correct files.')
 
@@ -112,9 +118,10 @@ def kneser_ney_probability(train_count, unicounts, bicounts):
 	return p1, p2
 
 def validation(validation_file, p1, p2, unicounts):
-	validation_count, line_count = 0, 0
+	line_count = 0
 	general_pp1, general_pp2 = 0, 0
 	for line in validation_file:
+		validation_count = 0
 		line_count += 1
 		pp1,pp2 = 0,0
 		words = line.strip().split(' ')
@@ -178,58 +185,70 @@ def kneser_ney_smoothing(validation_file, train_count, unicounts, bicounts):
 	pp1, pp2 = validation(validation_file, p1, p2, unicounts)
 	print('In general, pp1 = {}, pp2 = {}.'.format(pp1, pp2))
 
-def test(training_file_path1, training_file_path2, test_file_path1, test_file_path2, class1, class2, y1, y2):
-	with open(training_file_path1) as training_file1:
-		train_count1, unicounts1, bicounts1 = word_counter(training_file1)
-		p11, p12 = kneser_ney_probability(train_count1, unicounts1, bicounts1)
-	with open(training_file_path2) as training_file2:
-		train_count2, unicounts2, bicounts2 = word_counter(training_file2)
-		p21, p22 = kneser_ney_probability(train_count2, unicounts2, bicounts2)
-	with open(test_file_path1) as test_file1:
-		acc1 = prediction(test_file1, class1, class2, y1, p11, p12, p21, p22, unicounts1, unicounts2)
-	with open(test_file_path2) as test_file2:
-		acc2 = prediction(test_file2, class1, class2, y2, p11, p12, p21, p22, unicounts1, unicounts2)
-	acc_rate = {}
-	for r in np.linspace(0.96, 0.98, 11):
-		acc_rate[r] = acc1.get(r, 0) * 0.5 + acc2.get(r, 0) * 0.5
-		print('{:.3f}, {}'.format(r, acc_rate[r]))
-	r_max = max(acc_rate.keys(), key=(lambda k: acc_rate[k]))
-	print('The rate that maximizes accuracy is {}: {} %'.format(r_max, acc_rate[r_max] * 100))
+def accuracy_training(training_file_path1, training_file_path2, validation_file_path1, validation_file_path2, class1, class2, y1, y2):
+	for k in np.linspace(0.1, 1, 10):
+		print('k = {}'.format(k))
+		with open(training_file_path1) as training_file1:
+			train_count1, unicounts1, bicounts1 = word_counter(training_file1)
+			#p11, p12 = kneser_ney_probability(train_count1, unicounts1, bicounts1)
+			p11, p12 = add_k_probability(train_count1, unicounts1, bicounts1, k)
+		with open(training_file_path2) as training_file2:
+			train_count2, unicounts2, bicounts2 = word_counter(training_file2)
+			#p21, p22 = kneser_ney_probability(train_count2, unicounts2, bicounts2)
+			p21, p22 = add_k_probability(train_count2, unicounts2, bicounts2, k)
+		r_range = np.linspace(0.8, 1.0, 21)
+		acc_rate = {}	
+		with open(validation_file_path1) as validation_file1:
+			acc1 = accuracy(validation_file1, class1, class2, y1, p11, p12, p21, p22, unicounts1, unicounts2, r_range)
+		with open(validation_file_path2) as validation_file2:
+			acc2 = accuracy(validation_file2, class1, class2, y2, p11, p12, p21, p22, unicounts1, unicounts2, r_range)
+		for r in r_range:
+			acc_rate[r] = acc1.get(r, 0) * 0.5 + acc2.get(r, 0) * 0.5
+			print('{:.3f}, {}'.format(r, acc_rate[r]))
+		r_max = max(acc_rate.keys(), key=(lambda k: acc_rate[k]))
+		print('The rate that maximizes accuracy is {}: {} %'.format(r_max, acc_rate[r_max] * 100))
 
-def prediction(test_file, class1, class2, y, p11, p12, p21, p22, unicounts1, unicounts2):
-	line_count, test_word_count = 0, 0
-	accuracy_count, accuracy = {}, {}
+def accuracy(test_file, class1, class2, y, p11, p12, p21, p22, unicounts1, unicounts2, r_range):
+	line_count = 0
+	accuracy_count, acc = {}, {}
 	# print('Id, Prediction')
 	for line in test_file:
-		pp11, pp12, pp21, pp22 = 0, 0, 0, 0
-		words = line.strip().split(' ')
-		test_word_count += 1
-		pp11 += unigram_test(words[0].lower(), unicounts1, p11)
-		pp21 += unigram_test(words[0].lower(), unicounts2, p21)
-		for i in range(1, len(words)):
-			test_word_count += 1
-			pp11 += unigram_test(words[i].lower(), unicounts1, p11)
-			pp21 += unigram_test(words[i].lower(), unicounts2, p21)
-			pp12 += bigram_test(words[i - 1].lower(), words[i].lower(), unicounts1, p12)
-			pp22 += bigram_test(words[i - 1].lower(), words[i].lower(), unicounts2, p22)
-		pp11 = np.exp(- pp11 / test_word_count)
-		pp21 = np.exp(- pp21 / test_word_count)
-		k = 2
-		for r in np.linspace(0.96, 0.98, 11):
-			if r * (k * pp11 + (1 - k) * pp12) <= k * pp21 + (1 - k) * pp22:
+		pp11, pp21, pp12, pp22 = calculate_perplexity(line, p11, p12, p21, p22, unicounts1, unicounts2)
+		# print(line_count, pp11, pp21, pp12, pp22)
+		for r in r_range:
+			if r * pp12 <= pp22:
 				pred = class1
 			else: 
 				pred = class2
-			# print('For line {}: \npp11 = {:.2f}, pp21 = {:.2f}\npp12 = {:.2f}, pp22 = {:.2f}'.format(line_count, pp11, pp21, pp12, pp22))
-			# print('{}, {}'.format(line_count, pred))
+				# print('{}, {}'.format(line_count, pred))
 			if pred == y:
 				accuracy_count[r] = accuracy_count.get(r, 0) + 1
 		line_count += 1
-	# print('class1:{}, class2:{}, y:{}\nr, accuracy'.format(class1, class2, y))
-	for r in np.linspace(0.96, 0.98, 11):
-		# print('{:.2f}, {}'.format(r, float(accuracy_count.get(r, 0)) / line_count))
-		accuracy[r] = float(accuracy_count.get(r, 0)) / line_count
-	return accuracy
+	for r in r_range:
+		acc[r] = float(accuracy_count.get(r, 0)) / line_count
+	return acc
+
+def calculate_perplexity(line, p11, p12, p21, p22, unicounts1, unicounts2):
+	pp11, pp12, pp21, pp22 = 0, 0, 0, 0
+	test_word_count = 0
+	words = line.strip().split(' ')
+	test_word_count += 1
+	pp11 += unigram_test(words[0].lower(), unicounts1, p11)
+	pp21 += unigram_test(words[0].lower(), unicounts2, p21)
+	for i in range(1, len(words)):
+		test_word_count += 1
+		pp11 += unigram_test(words[i].lower(), unicounts1, p11)
+		pp21 += unigram_test(words[i].lower(), unicounts2, p21)
+		pp12 += bigram_test(words[i - 1].lower(), words[i].lower(), unicounts1, p12)
+		pp22 += bigram_test(words[i - 1].lower(), words[i].lower(), unicounts2, p22)
+	pp11 = np.exp(- pp11 / test_word_count)
+	pp21 = np.exp(- pp21 / test_word_count)
+	pp12 = np.exp(- pp12 / test_word_count)
+	pp22 = np.exp(- pp22 / test_word_count)
+	return pp11, pp21, pp12, pp22
+
+def test(training_file_path1, training_file_path2, test_file_path, class1, class2):
+	print('to do')
 
 if __name__ == '__main__':
    main()
