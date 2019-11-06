d6c1bed731a3aa20ecb10332be074f772d75aabc
YangZ <zymy701@gmail.com>
2019-11-05 20:39:03 -0500

    original

---
+++ P3-FFNN-RNN/main.py
@@ -0,0 +1,21 @@
+from rnn import main as rnn_main
+from ffnn1fix import main as ffnn_main
+
+
+FLAG = 'FFNN'
+
+
+def main():
+	if FLAG == 'RNN':
+		raise NotImplementedError
+		rnn_main()
+	elif FLAG == 'FFNN':
+		hidden_dim = 32
+		# number_of_epochs = 10
+		number_of_epochs = 1
+		ffnn_main(hidden_dim=hidden_dim, number_of_epochs=number_of_epochs)
+
+
+
+if __name__ == '__main__':
+	main()
