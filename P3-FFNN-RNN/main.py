899d52ab784b913400b0b327a6d7f1dfa7093555
YangZ <zymy701@gmail.com>
2019-12-08 23:50:10 -0500

    P4-B

--- P3-FFNN-RNN/main.py
+++ P3-FFNN-RNN/main.py
@@ -2,23 +2,21 @@ from rnn import main as rnn_main
 from ffnn1fixed import main as ffnn_main
 
 
-FLAG = 'FFNN'
+FLAG = 'RNN'
 
 
 def main():
 	if FLAG == 'RNN':
-		input_size = 128
+		input_size = 50
 		hidden_size = 32
-		number_of_epochs = 1
+		number_of_epochs = 5
 		minibatch_size = 16
 		rnn_main(input_size, hidden_size, number_of_epochs, minibatch_size)
 	elif FLAG == 'FFNN':
 		hidden_dim = 32
-		number_of_epochs = 1
+		number_of_epochs = 5
 		minibatch_size = 16
 		ffnn_main(hidden_dim, number_of_epochs, minibatch_size)
-
-
 
 if __name__ == '__main__':
 	main()
